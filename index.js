932f32132e5510825e070cb478a816292210ed74
missoum1307 <missoum1307@users.noreply.github.com>
2023-02-04 18:39:02 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,4 +5,16 @@ app.get("/", function (req, res) {
   res.send(req.query.input);
 });
 
+app.get("/google-accounts-xss.html", function (req, res) {
+  res.sendFile("google-accounts-xss.html");
+});
+
+app.get("/google-csrfs.html", function (req, res) {
+  res.sendFile("google-csrfs.html");
+});
+
+app.get("/googlebugs.html", function (req, res) {
+  res.sendFile("googlebugs.html");
+});
+
 app.listen(3000);
