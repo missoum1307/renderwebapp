a2d75a47caf7efdb128868d8b28f3f5c0360d682
missoum1307 <missoum1307@users.noreply.github.com>
2023-02-04 18:52:26 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const app = express();
+var path = require("path");
 
 app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/input", function (req, res) {
   res.send(req.query.input);
 });
 
 app.get("/google-accounts-xss.html", function (req, res) {
-  res.sendFile("google-accounts-xss.html");
+  res.sendFile(path.join(__dirname, "google-accounts-xss.html"));
 });
 
 app.get("/google-csrfs.html", function (req, res) {
-  res.sendFile("google-csrfs.html");
+  res.sendFile(path.join(__dirname, "google-csrfs.html"));
 });
 
 app.get("/googlebugs.html", function (req, res) {
-  res.sendFile("googlebugs.html");
+  res.sendFile(path.join(__dirname, "googlebugs.html"));
 });
 
-app.listen(3000);
+app.listen(3003);
